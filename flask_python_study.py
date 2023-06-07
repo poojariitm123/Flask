a79c998b55ada9d28667e007aa52e2bcf855780d
poojariitm123 <135839303+poojariitm123@users.noreply.github.com>
2023-06-07 18:14:47 +0530

    Add files via upload

---
+++ flask_python_study.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Sample data
+books = [
+    {"id": 1, "title": "Book 1", "author": "Author 1"},
+    {"id": 2, "title": "Book 2", "author": "Author 2"},
+    {"id": 3, "title": "Book 3", "author": "Author 3"}
+]
+
+# HTTP GET
+@app.route('/books', methods=['GET'])
+def get_books():
+    return jsonify(books)
+
+# HTTP GET - Get a specific book
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    for book in books:
+        if book['id'] == book_id:
+            return jsonify(book)
+    return jsonify({'message': 'Book not found'}), 404
+
+# HTTP POST - Add a new book
+@app.route('/books', methods=['POST'])
+def add_book():
+    new_book = {
+        'id': request.json['id'],
+        'title': request.json['title'],
+        'author': request.json['author']
+    }
+    books.append(new_book)
+    return jsonify(new_book), 201
+
+# HTTP PUT - Update an existing book
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    for book in books:
+        if book['id'] == book_id:
+            book['title'] = request.json['title']
+            book['author'] = request.json['author']
+            return jsonify(book)
+    return jsonify({'message': 'Book not found'}), 404
+
+# HTTP DELETE - Delete a book
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            return jsonify({'message': 'Book deleted'})
+    return jsonify({'message': 'Book not found'}), 404
+
+if __name__ == '__main__':
+    app.run()
